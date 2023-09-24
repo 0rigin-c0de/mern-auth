255b18a63808f4d76f82e84d2197045de3bca3d0
0rigin-c0de <shunnu807@gmail.com>
2023-09-24 19:50:36 +0530

    initial-commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Container, Card, Button } from "react-bootstrap";
 
 const Hero = () => {
@@ -7,9 +8,9 @@ const Hero = () => {
         <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
           <h1 className="text-center mb-4 text-primary">MERN Authentication</h1>
           <p className="text-center mb-4 text-secondary">
-            This is a boilerplate for MERN authentication that stores a JWT in
-            an HTTP-Only cookie. It also uses Redux Toolkit and the React
-            Bootstrap library
+            Welcome to the MERN Authentication boilerplate, where JWT tokens are
+            securely stored in HTTP-Only cookies. This project leverages Redux
+            Toolkit and the React Bootstrap library.
           </p>
           <div className="d-flex">
             <Button variant="primary" href="/login" className="me-3">
